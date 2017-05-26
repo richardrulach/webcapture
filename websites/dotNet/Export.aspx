d95e1d888aa746d86d0ff71893935e628fffeb56
Richard <richardrulach@gmail.com>
2017-05-26 17:09:22 +0100

    Added Export page for question and answer

---
+++ websites/dotNet/Export.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Export.aspx.cs" Inherits="Export" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <script type="text/javascript" src="_js/jquery.js"></script>
+
+
+    <script type="text/javascript">
+
+        $(document).ready(function () {
+            copyToClipboard($('#txtOutput'));
+        });
+
+        function copyToClipboard(element) {
+            var $temp = $("<input>");
+            $("body").append($temp);
+            $temp.val($(element).text()).select();
+            document.execCommand("copy");
+            $temp.remove();
+        }
+    </script>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div id="txtOutput">
+        <asp:Literal ID="LitOutput" runat="server" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ websites/dotNet/Export.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+
+public partial class Export : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+        String output = string.Empty;
+
+        string liveConnection = @"Server=.\localtest;Database=Cards;User Id=richard;Password=richard;";
+
+        SqlConnection sqlconnection = new SqlConnection(liveConnection);
+        sqlconnection.Open();
+
+        SqlCommand sqlcommand = new SqlCommand();
+        sqlcommand.Connection = sqlconnection;
+
+        sqlcommand.CommandText = "select answer, question from [text] order by textid asc";
+
+        SqlDataReader dr = sqlcommand.ExecuteReader();
+
+
+        while (dr.Read())
+        {
+            output += dr[0].ToString() + "|" + dr[1].ToString() + "¬";
+        }
+
+        dr.Close();
+        sqlcommand.Dispose();
+        sqlconnection.Close();
+
+        LitOutput.Text = output;
+    }
+
+
+
+}
